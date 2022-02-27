a5d5db234f26a6d3edbe317fc2dffb031a00c41e
Donggyu Kim <ptsaturn68@daum.net>
2022-02-27 17:20:12 +0900

    [feat] 상수 등록

---
+++ src/javascript/constants/const.js
@@ -0,0 +1,11 @@
+export const MAX_CHANCE_NUM = 6;
+export const MAX_TILE_NUM = 5;
+export const ALPHA_NUM = 26;
+
+export const FIT = 3;
+export const EXIST = 2;
+export const WRONG = 1;
+export const BEFORE = 0;
+
+export const ONGOING = 1;
+export const FINISHED = 0;
