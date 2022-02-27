a030000b73f315c48d841e2e59f61a8766c4246f
Donggyu Kim <ptsaturn68@daum.net>
2022-02-27 20:29:09 +0900

    [fix] 상수 수정 및 추가

--- src/javascript/constants/const.js
+++ src/javascript/constants/const.js
@@ -2,10 +2,11 @@ export const MAX_CHANCE_NUM = 6;
 export const MAX_TILE_NUM = 5;
 export const ALPHA_NUM = 26;
 
-export const FIT = 3;
-export const EXIST = 2;
-export const WRONG = 1;
-export const BEFORE = 0;
+export const FIT = "fit";
+export const EXIST = "exist";
+export const WRONG = "wrong";
+export const BEFORE = "before";
 
+export const CORRECT = 2;
 export const ONGOING = 1;
 export const FINISHED = 0;
